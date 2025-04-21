66b39d72cf3c7fa15196519369c43504884c8be3
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-21 19:05:13 -0300

    style: icon to header

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -3,6 +3,7 @@ export default function Header() {
     return (
         <>
             <div className="header">
+                <img width="50px" src="/src/assets/curriculum-vitae.svg"></img>
                 <h1>CV Builder</h1>
             </div>
         </>
