e306a94844e2c2cef699d9db2543147aa30deb9e
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-20 21:29:28 -0300

    feat: footer component

---
+++ src/components/sections/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+
+    return (
+        <>  
+        <div className="footer">
+            <p>Copyright © Luis Octávio</p>
+        </div>
+    </>
+    )
+   
+}
