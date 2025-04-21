f578fd9afdc5565a6e859d8ee8574879b6464ec0
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-21 19:34:51 -0300

    fix: adjust the name of the two fieldsets individually

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import Field from "./Field.jsx"
 
-export default function Menu({ title }) {
+export default function Menu({ title, textOne, textTwo }) {
     const [isOpen, setIsOpen] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -31,19 +32,26 @@ export default function Menu({ title }) {
             {isModalOpen && (
                 <div className="modal">
                     <div className="modal-content">
-                        <h3>Conteudo</h3>
-                        <p>Este modal funciona</p>
-                        <button 
-                        onClick={() => 
-                            {
-                                setIsOpen(false)
-                                setIsModalOpen(false)
+                        <Field type="text" text={textOne} />
+                        <Field type="text" text={textTwo} />
+                        <Field type="date" text="Start Date" width="50px"/>
+                        <Field type="date" text="End Date" />
+                        <div className="button-menu-div">
+                            <button className="submit-button" type="submit">
+                            Create
+                            </button>
+                            <button 
+                            onClick={() => 
+                                {
+                                    setIsOpen(false)
+                                    setIsModalOpen(false)
+                                }
                             }
-                        }
-                        className="submit-button"
-                        >
-                        Close
-                        </button>
+                            className="submit-button"
+                            >
+                            Close
+                            </button>
+                        </div>
                     </div>
                 </div>
             )}
