90cfb2aa2d15b15ea196cc81c0f4d0241a095cb4
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-20 21:29:42 -0300

    feat: page section component

---
+++ src/components/sections/Page.jsx
@@ -0,0 +1,12 @@
+import Button from "../Button.jsx";
+
+export default function Page(props) {
+    return (
+        <div className="cv-page-bg">
+            <div className="cv-page">
+
+            </div>
+            <Button />
+        </div>
+    )
+}
