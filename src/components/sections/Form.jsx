0e4dafe26ddbeba8490ceb3c0f84884dfb7c3bd9
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-21 20:35:05 -0300

    feat: hardcoded the template of page section

--- src/components/sections/Form.jsx
+++ src/components/sections/Form.jsx
@@ -12,32 +12,44 @@ export default function Form() {
                         text="First Name"
                         type="text" 
                         placeholder="John"
+                        name="firstname"
                     />
 
                     <Field 
                         text="Last Name"
                         type="text"
                         placeholder="Doe"
+                        name="secondname"
                     />
                     
                     <Field 
                         text="E-mail"
                         type="email"
                         placeholder="john.doe@example.com"
+                        pattern="/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim"
+                        name="email"
                     />
 
                     <Field
                         text="LinkedIn"
                         type="text"
                         placeholder="linkedin.com/in/john-doe/"
+                        pattern="/^(https?:\/\/)?(www\.)?linkedin\.com\/in\/[a-zA-Z0-9\-_]+\/?$/"
+                        name="linkedin"
                     />
 
                     <Field
                         text="Telephone"
+                        type="tel"
+                        placeholder="+55 (00) 92567-6578"
+                        pattern="/^\+\d{2}\s\(\d{2}\)\s\d{5}-\d{4}$/"
+                        name="telephone"
                     />
 
                     <Field 
                         text="Address"
+                        placeholder="Street Marginal Santos City, State"
+                        pattern="^[A-Za-zÀ-ÿ0-9\s]+,\s*[A-Za-zÀ-ÿ\s]+,\s*[A-Za-zÀ-ÿ]{2}$"
                     />
                 </fieldset>
 
