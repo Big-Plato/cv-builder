3d869db3d78508b3a05f0427b1593db3db1e97ac
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-22 20:47:44 -0300

    fix: manipulate input data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import { useState } from 'react'
-import './App.css'
-import Button from "./components/Button"
-import Form from './components/sections/Form.jsx'
-import Page from './components/sections/Page.jsx'
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/sections/Form.jsx";
+import Page from "./components/sections/Page.jsx";
 
-function App() {
+export default function App() {
+  const [formData, setFormData] = useState({
+    firstname: "",
+    secondname: "",
+    email: "",
+    linkedin: "",
+    telephone: "",
+    university: "",
+    course: "",
+    startCourse: "",
+    endCourse: "",
+    company: "",
+    job: "",
+    resume: "",
+    initialJob: "",
+    endJob: "",
+  });
+
+  const handleFormChange = (updatedData) => {
+    setFormData(updatedData);
+    console.log(updatedData);
+  };
+
   return (
-    <>
-    <div className='main'>
-      <Form />
-      <Page />
+    <div className="main">
+      <Form inputs={formData} onFormChange={handleFormChange} />
+      <Page formData={formData} />
     </div>
-    </>
-  )
+  );
 }
-
-export default App
