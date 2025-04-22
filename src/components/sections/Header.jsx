45e273150031d1074fa315db92a6a30bdd8d5967
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-22 20:49:47 -0300

    feat: dark mode

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,11 +1,37 @@
+import Button from "../Button.jsx";
 
 export default function Header() {
-    return (
-        <>
-            <div className="header">
-                <img width="50px" src="/src/assets/curriculum-vitae.svg"></img>
-                <h1>CV Builder</h1>
-            </div>
-        </>
-    )
+  const body = document.querySelector('body');
+  body.setAttribute('data-theme', 'light')
+  const setDarkMode = () => {
+    body.setAttribute('data-theme', 'dark');
+  }
+
+  const setLightMode = () => {
+    body.setAttribute('data-theme', 'light');
+  }
+
+  const toggleTheme = (e) => {
+    if (body.getAttribute('data-theme') === 'dark') {
+      console.log('ok')
+      setLightMode();
+    } else {
+      setDarkMode();
+    }
+  }
+
+  return (
+    <>
+      <div className="header">
+        <div>
+          <img width="50px" src="/src/assets/curriculum-vitae.svg"></img>
+          <h1>CV Builder</h1>
+        </div>
+        <Button 
+          className='theme'
+          onChange={toggleTheme}
+        />
+      </div>
+    </>
+  );
 }
