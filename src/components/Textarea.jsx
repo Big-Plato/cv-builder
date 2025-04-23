5621bf5c94382ad5b488a3170e9d052c2f1189fd
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-22 22:28:32 -0300

    style: indent with prettier

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,12 +1,12 @@
-export default function Textarea ({ name, value, placeholder, onChange}) {
-    return (
-        <div>
-            <textarea
-            name={name}
-            value={value}
-            placeholder={placeholder}
-            onChange={onChange}>
-            </textarea>
-        </div>
-    )
+export default function Textarea({ name, value, placeholder, onChange }) {
+  return (
+    <div>
+      <textarea
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+      ></textarea>
+    </div>
+  );
 }
